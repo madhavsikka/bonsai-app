78e3d92083ac6c3b45625bc24fcaac308db1dbad
madhavsikka <madhavsikka99@gmail.com>
2024-01-30 12:36:46 +0530

    add card height

--- src/components/leaf/LeafCard.tsx
+++ src/components/leaf/LeafCard.tsx
@@ -4,19 +4,25 @@ import { patterns } from './patterns';
 
 interface LeafCardProps extends React.HTMLAttributes<HTMLDivElement> {
   leaf: Leaf;
+  coverImageClassName?: string;
 }
 
 const getRandomPattern = () => {
   return patterns[Math.floor(Math.random() * patterns.length)];
 };
 
-export function LeafCard({ className, leaf, ...props }: LeafCardProps) {
+export function LeafCard({
+  className,
+  coverImageClassName,
+  leaf,
+  ...props
+}: LeafCardProps) {
   return (
-    <div className="flex flex-col flex-grow justify-between">
+    <div className={cn('flex flex-col flex-grow justify-between', className)}>
       <div
         className={cn(
-          'space-y-3 rounded-t-lg h-32 border border-b-0',
-          className
+          'space-y-3 rounded-t-lg border border-b-0',
+          coverImageClassName
         )}
         {...props}
         style={{ ...getRandomPattern() }}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import { Sidebar } from '@/components/sidebar/Sidebar';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { useListLeafs } from '@/hooks/leaf/useListLeafs';
 import { Link } from 'react-router-dom';
 import { LeafCard } from '@/components/leaf/LeafCard';
@@ -15,7 +14,7 @@ export const HomePage = () => {
       <Menu />
       <div className="flex border-t mt-2 h-full">
         <Sidebar className="min-w-[300px] h-full" />
-        <div className="col-span-3 lg:col-span-4 lg:border-l flex-grow">
+        <div className="col-span-2 lg:border-l flex-grow">
           <div className="h-full px-4 py-6 lg:px-8">
             <Tabs defaultValue="leafs" className="h-full space-y-6">
               <div className="space-between flex items-center">
@@ -29,18 +28,18 @@ export const HomePage = () => {
                 value="leafs"
                 className="border-none p-0 outline-none"
               >
-                <div className="relative">
-                  <ScrollArea>
-                    <div className="flex space-x-4 pb-4">
-                      {!loading &&
-                        leafs.map((leaf) => (
-                          <Link to={`/leafs/${leaf.id}`} key={leaf.id}>
-                            <LeafCard leaf={leaf} key={leaf.id} />
-                          </Link>
-                        ))}
-                    </div>
-                    <ScrollBar orientation="horizontal" />
-                  </ScrollArea>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8">
+                  {!loading &&
+                    leafs.map((leaf) => (
+                      <Link to={`/leafs/${leaf.id}`} key={leaf.id}>
+                        <LeafCard
+                          leaf={leaf}
+                          key={leaf.id}
+                          className="min-h-[500px]"
+                          coverImageClassName="min-h-[200px]"
+                        />
+                      </Link>
+                    ))}
                 </div>
               </TabsContent>
             </Tabs>
