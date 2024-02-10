93abd0901b1bd214fddc5167a0f51fc332cca019
madhavsikka <madhavsikka99@gmail.com>
2024-02-10 17:05:50 +0530

    bump

--- src-tauri/src/cozo.rs
+++ src-tauri/src/cozo.rs
@@ -15,68 +15,50 @@ pub fn init() {
     let bert = BertModelEmbeddings::new(bert_args).unwrap();
 
     let mut script = "
-    :create leaf {
+    {:create leaf {
         id 
         => 
-        title, 
-        body, 
-        title_vec: <F32; 384>, 
-        body_vec: <F32; 384>
-    }
+        title,
+        title_vec: <F32; 384>
+    }}
+    {::hnsw create leaf:semantic{
+        fields: [title_vec], 
+        dim: 384, 
+        ef: 16, 
+        m: 32,
+        distance: Cosine
+    }}
     ";
     let mut result = db.run_default(script).unwrap();
     println!("{:?}", result);
 
-    script = "
-    ::hnsw create leaf:semantic{
-        fields: [title_vec, body_vec], 
-        dim: 384, 
-        ef: 16, 
-        m: 32
-    }
-    ";
-    result = db.run_default(script).unwrap();
-    println!("{:?}", result);
-
     let records = vec![
-        vec!["1", "i am a good driver", "how are you"],
-        vec!["2", "i drive well", "woohoo"],
-        vec!["3", "sun is out there", "cool"],
+        vec!["1", "This is an HNSW (hierarchical navigable small world) vector index, and ef and m are parameters that control the quality-performance trade-off of the index. Now when inserting rows for the product table, we use an embedding model (such as text-embedding-ada-002 provided by OpenAI) to compute embeddings for the texts and insert them together with the other fields. Now an iPhone and a Samsung phone are related even without a manually curated knowledge graph."],
+        vec!["2", "Lets go for a basketball game tonight."],
+        vec!["3", "I am all for wildlife."],
+        vec!["4", "I am going to the nba game tonight."],
     ];
 
     for record in records {
         let id = record[0].to_string();
         let title = record[1].to_string();
-        let body = record[2].to_string();
-        let input = vec![title.clone(), body.clone()];
+        let input = vec![title.clone()];
         let embeddings = bert.embed(input).unwrap();
         let title_vec = embeddings[0].to_vec();
-        let body_vec = embeddings[1].to_vec();
         let script = format!(
-            "?[id, title, body, title_vec, body_vec] <- [['{}', '{}', '{}', {:?}, {:?}]]
-            :put leaf {{id => title, body, title_vec, body_vec}}
+            "?[id, title, title_vec] <- [['{}', '{}', {:?}]]
+            :put leaf {{id => title, title_vec}}
             ",
-            id, title, body, title_vec, body_vec
+            id, title, title_vec
         );
         db.run_default(&script).unwrap();
     }
 
     script = "
-    ?[id, title, body, title_vec, body_vec] := *leaf{id, title, body, title_vec, body_vec}
+    ?[dist, fr_id, to_id] :=
+        *leaf:semantic{layer: 0, dist, fr_id, to_id},
     ";
     result = db.run_default(script).unwrap();
-    println!("{:?}", result);
-
-    script = "
-    ?[dist, title] :=
-    *leaf{title: 'i am a good driver', title_vec: v},
-    ~leaf:semantic{title | query: v, bind_distance: dist, k: 5, ef: 20}
-
-    :order dist
-    :limit 5
-    ";
-    result = db.run_default(script).unwrap();
-
     println!("{:?}", result);
 }
 
--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,5 +1,6 @@
-// use std::fs;
-// use std::path::Path;
+use cozo::*;
+use std::fs;
+use std::path::Path;
 
 // use diesel::prelude::*;
 // use diesel::sqlite::SqliteConnection;
@@ -7,13 +8,26 @@
 
 // const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
-// pub fn init() {
-//     if !db_file_exists() {
-//         create_db_file();
-//     }
+pub fn init() {
+    if !db_file_exists() {
+        create_db_file();
+    }
+    let db = establish_connection();
 
-//     run_migrations();
-// }
+    _ = db.run_default(
+        "
+        :create leaf {
+            id 
+            => 
+            title,
+            body,
+            body_vec: <F32; 384>
+        }
+    ",
+    );
+
+    // run_migrations();
+}
 
 // pub fn establish_db_connection() -> SqliteConnection {
 //     let db_path = get_db_path().clone();
@@ -27,30 +41,29 @@
 //     connection.run_pending_migrations(MIGRATIONS).unwrap();
 // }
 
-// fn establish_connection() -> SqliteConnection {
-//     let db_path = "sqlite://".to_string() + get_db_path().as_str();
+pub fn establish_connection() -> DbInstance {
+    let db_path = get_db_path();
+    DbInstance::new("sqlite", db_path.clone(), Default::default())
+        .unwrap_or_else(|_| panic!("Error connecting to {}", db_path))
+}
 
-//     SqliteConnection::establish(&db_path)
-//         .unwrap_or_else(|_| panic!("Error connecting to {}", db_path))
-// }
+fn create_db_file() {
+    let db_path = get_db_path();
+    let db_dir = Path::new(&db_path).parent().unwrap();
 
-// fn create_db_file() {
-//     let db_path = get_db_path();
-//     let db_dir = Path::new(&db_path).parent().unwrap();
+    if !db_dir.exists() {
+        fs::create_dir_all(db_dir).unwrap();
+    }
 
-//     if !db_dir.exists() {
-//         fs::create_dir_all(db_dir).unwrap();
-//     }
+    fs::File::create(db_path).unwrap();
+}
 
-//     fs::File::create(db_path).unwrap();
-// }
+fn db_file_exists() -> bool {
+    let db_path = get_db_path();
+    Path::new(&db_path).exists()
+}
 
-// fn db_file_exists() -> bool {
-//     let db_path = get_db_path();
-//     Path::new(&db_path).exists()
-// }
-
-// fn get_db_path() -> String {
-//     let home_dir = dirs::home_dir().unwrap();
-//     home_dir.to_str().unwrap().to_string() + "/.config/orion/database.sqlite"
-// }
+fn get_db_path() -> String {
+    let home_dir = dirs::home_dir().unwrap();
+    home_dir.to_str().unwrap().to_string() + "/bonsai/bonsai.db"
+}
