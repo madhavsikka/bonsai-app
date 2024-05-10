a38e9f2c9d86323beb449666c0c09c342e63a0c8
madhavsikka <madhavsikka99@gmail.com>
2024-05-10 11:23:40 +0530

    fix nav highlighting

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,28 +1,34 @@
+import { useNavigate, useLocation } from 'react-router-dom';
 import { BonsaiAvatar } from '../avatar/BonsaiAvatar';
 import { Leaf } from 'lucide-react';
 
 export const Sidebar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div className="hidden border-r md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-          <a
-            href="/"
-            className="flex items-center justify-center gap-2 font-semibold"
-          >
-            <BonsaiAvatar />
-            <span className="text-md font-normal">bonsai</span>
-          </a>
+        <div
+          className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 gap-2 cursor-pointer"
+          onClick={() => navigate('/')}
+        >
+          <BonsaiAvatar />
+          <span className="text-md font-normal">bonsai</span>
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <a
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-foreground transition-all hover:text-primary bg-muted hover:bg-muted-100"
+            <div
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 text-foreground transition-all hover:text-primary cursor-pointer ${
+                location.pathname === '/'
+                  ? 'bg-muted text-primary'
+                  : 'hover:bg-muted-100'
+              }`}
+              onClick={() => navigate('/')}
             >
               <Leaf className="h-4 w-4" />
               Leaves
-            </a>
+            </div>
           </nav>
         </div>
       </div>
